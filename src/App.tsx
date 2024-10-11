b23ca68208b771391728439f398cfbe2c472c39f
kdog3682 <kdog3682@gmail.com>
2024-10-11 15:30:30 -0400

    [working example] simple tree view
    forked from https://zweifisch.github.io/lezer-ast-explorer/

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,95 @@
-import { StyleXExample } from './components/StyleXExample.tsx'
+import * as React from 'react'
+import { Editor } from './components/Editor'
+import * as placeholders from './components/placeholders'
+import { javascript } from '@codemirror/lang-javascript'
+import { markdown } from '@codemirror/lang-markdown'
 
-export default function App() {
-    return (
-        <div>
-            <StyleXExample />
+
+const languages = {
+    javascript: [javascript, { jsx: true, typescript: true }],
+    markdown: [markdown, {}],
+}
+
+export default function () {
+
+    const initialLang = React.useMemo(() => {
+        return localStorage.getItem('lang') || 'javascript'
+    }, [])
+
+    const [lang, setLang] = React.useState(initialLang)
+    const [code, setCode] = React.useState('')
+
+    const langExt = React.useMemo(() => {
+        const [factory, options] = languages[lang]
+        localStorage.setItem('lang', lang)
+        return factory(options)
+    }, [lang])
+
+    const handleEditorChange = (code: string) => {
+        setCode(code)
+        localStorage.setItem(lang, code)
+    }
+
+    React.useEffect(() => {
+        setCode(localStorage.getItem(lang) || placeholders[lang] || '')
+    }, [lang])
+
+    React.useEffect(() => {
+        document.title = 'Lezer AST Explorer'
+    }, [])
+
+    const style = `
+div::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+div::-webkit-scrollbar-track-piece {
+  background: #efefef;
+}
+
+div::-webkit-scrollbar-thumb {
+  background: #ccc;
+  border-radius: 2px;
+}
+
+html {
+  font-family: monospace;
+}
+
+.cm-focused {
+  outline: none !important;
+}
+  `
+
+    return <>
+
+        <style>{style}</style>
+
+        <div style={{ height: '100vh', overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
+            <div style={{ display: 'flex', gap: '10px', padding: '0', borderBottom: '1px solid #ccc' }}>
+                {
+                    Object.keys(languages).map(l =>
+                        <div
+                            style={{ background: lang === l ? '#fafafa' : '#efefef', padding: '10px 6px', cursor: 'pointer' }}
+                            key={l}
+                            onClick={() => setLang(l)}
+                        >
+                            {l}
+                        </div>
+                    )
+                }
+            </div>
+            <Editor key={lang} style={{ flexGrow: 1 }} lang={langExt} value={code} onChange={handleEditorChange} />
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', borderTop: '1px solid #ccc', padding: '6px', gap: '0.5em' }}>
+                Built with
+                <a href="https://react.dev/">React</a>
+                <a href="https://codemirror.net/">CodeMirror</a>
+                and
+                <a href="https://esbuild.github.io/">esbuild</a>
+                |
+                <a href="https://github.com/zweifisch/lezer-ast-explorer">GitHub</a>
+            </div>
         </div>
-    )
+    </>
 }
---
+++ src/components/placeholders.ts
@@ -0,0 +1,48 @@
+
+export const javascript = `
+console.log('Guten Tag.')
+`
+
+export const css = `
+.main {
+  color: #ff0000;
+}
+`
+
+export const less = `
+.main {
+  .content {
+    color: #ff0000;
+  }
+}
+`
+
+export const markdown = `
+# Title
+
+- markdown
+- org-mode
+`
+
+export const rust = `
+fn main() {
+  println!("Hallo");
+}
+`
+
+export const java = `
+public class Lezer {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+`
+
+export const xml = `\
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>`
